e1998042c1c8408939ef8cd9820b9a49ac0aea36
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2026-02-22 21:02:13 -0500

    vercel error correction 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import "./globals.css";
 
